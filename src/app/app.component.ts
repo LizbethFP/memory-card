54f7a24baf36ddb3c9e3175fcc98c981ee48f29a
LizbethFP <lizbeth.fp@gmail.com>
2018-04-21 17:02:05 -0500

    adds toggle cards w click angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,14 @@ export class AppComponent implements OnInit {
   data: any = {};
   public title: string;
   public cards: any;
+  showCard: boolean;
+  hideCard: boolean;
  
   constructor(private appService: AppService) {
     this.title = 'Cards Memory';
     this.cards = ['http://deckofcardsapi.com/static/img/AS.png', 'http://deckofcardsapi.com/static/img/AH.png', 'http://deckofcardsapi.com/static/img/8H.png', 'https://deckofcardsapi.com/static/img/8S.png', 'https://deckofcardsapi.com/static/img/7C.png', 'https://deckofcardsapi.com/static/img/7D.png', 'https://deckofcardsapi.com/static/img/QD.png', 'https://deckofcardsapi.com/static/img/QS.png'];
+    this.showCard = false;
+    this.hideCard = true;
   }
 
   ngOnInit(): void {
@@ -31,6 +35,27 @@ export class AppComponent implements OnInit {
       
       this.data = data     
     });
+  }
+
+  toggleCards() {
+    // this.showCard = true;
+    // this.hideCard = false;
+    this.showCard = !this.showCard;
+    this.hideCard = !this.hideCard;
+    console.log('toggleCards Works');    
+  }
+
+  toggleCardsBack() {
+    // this.showCard = true;
+    // this.hideCard = false;
+    this.showCard = !this.showCard;
+    this.hideCard = !this.hideCard;
+    console.log('toggleCardsBack Works');    
+  }
+
+  flipCards() {
+     let backCard = document.getElementsByClassName('back-card');
+     let frontCard = document.getElementsByClassName('front-card');
   }
 
 }
