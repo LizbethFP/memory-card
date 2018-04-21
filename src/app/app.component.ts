1969577ea6a2a01c8ef49cfd0db14380d1831fde
LizbethFP <lizbeth.fp@gmail.com>
2018-04-21 13:43:06 -0500

    adds style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ import { AppService } from './app.service';
 export class AppComponent implements OnInit { 
   data: any = {};
   public title: string;
-  public cards: Array<string>;
+  public cards: any;
  
   constructor(private appService: AppService) {
-    this.title = 'la try';
+    this.title = 'Cards Memory';
     this.cards = ['http://deckofcardsapi.com/static/img/AS.png', 'http://deckofcardsapi.com/static/img/AH.png', 'http://deckofcardsapi.com/static/img/8H.png', 'https://deckofcardsapi.com/static/img/8S.png', 'https://deckofcardsapi.com/static/img/7C.png', 'https://deckofcardsapi.com/static/img/7D.png', 'https://deckofcardsapi.com/static/img/QD.png', 'https://deckofcardsapi.com/static/img/QS.png'];
   }
 
