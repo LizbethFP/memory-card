fef3a9f6cda78fc23d2e875474fc00b633ac8412
LizbethFP <lizbeth.fp@gmail.com>
2018-04-22 10:34:37 -0500

    adds array pipe module for proper array shuffle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 
-const unShuffledCards = ['http://deckofcardsapi.com/static/img/AS.png', 'http://deckofcardsapi.com/static/img/AH.png', 'http://deckofcardsapi.com/static/img/8H.png', 'https://deckofcardsapi.com/static/img/8S.png', 'https://deckofcardsapi.com/static/img/7C.png', 'https://deckofcardsapi.com/static/img/7D.png', 'https://deckofcardsapi.com/static/img/QD.png', 'https://deckofcardsapi.com/static/img/QS.png'];
-
-// const shuffledCards = () => {
-//   let temp = [];
-//   // temp.push(unShuffledCards[Math.floor(Math.random() * unShuffledCards.length)]);
-//   for (var i = 0; i < unShuffledCards.length; i++) {
-//     temp.push(Math.floor(Math.random() * unShuffledCards.length));
-//   }
-//   console.log(temp);
-//   return temp;
-// };
-
-const shuffledCards = (arra1) => {
-  var ctr = arra1.length, temp, index;
-
-  // While there are elements in the array
-  while (ctr > 0) {
-    // Pick a random index
-    index = Math.floor(Math.random() * ctr);
-    // Decrease ctr by 1
-    ctr--;
-    // And swap the last element with it
-    temp = arra1[ctr];
-    arra1[ctr] = arra1[index];
-    arra1[index] = temp;
-  }
-  console.log(arra1);
-  return arra1;
-}
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -44,17 +13,15 @@ const shuffledCards = (arra1) => {
 export class AppComponent implements OnInit { 
   data: any = {};
   public title: string;
-  // public unShuffledCards: Array<string>;
-  // public shuffledCards: any;
   public cards: Array<string>;
   showCard: boolean;
   hideCard: boolean;
  
   constructor(private appService: AppService) {
     this.title = 'Cards Memory';
-    this.cards = shuffledCards(unShuffledCards);
     this.showCard = false;
     this.hideCard = true;
+    this.cards = ['http://deckofcardsapi.com/static/img/AS.png', 'http://deckofcardsapi.com/static/img/AH.png', 'http://deckofcardsapi.com/static/img/8H.png', 'https://deckofcardsapi.com/static/img/8S.png', 'https://deckofcardsapi.com/static/img/7C.png', 'https://deckofcardsapi.com/static/img/7D.png', 'https://deckofcardsapi.com/static/img/QD.png', 'https://deckofcardsapi.com/static/img/QS.png'];
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 // Importar HttpClientModule
 import { HttpClientModule } from '@angular/common/http';
+import { NgArrayPipesModule } from 'ngx-pipes';
+
 
 
 @NgModule({
@@ -12,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule, // cargamos el módulo en el array de imports
+    NgArrayPipesModule // cargamos el módulo de shuffle en array
   ],
   providers: [],
   bootstrap: [AppComponent]
